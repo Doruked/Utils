0a90b61d53ca19b8468746decff80cd44ac5e328
Doruked <44949043+Doruked@users.noreply.github.com>
2020-06-03 23:33:01 -0700

    Refactor class to be more accurate
    shouldn't be called lambda if it returns
    something; it is a supplier

--- src/main/java/com/example/doruked/SupplierEX.java
+++ src/main/java/com/example/doruked/SupplierEX.java
@@ -11,8 +11,7 @@ package com.example.doruked;
 
 //don't feel the need to actually wrap as optional given that when people
 //call, it will be through a lambda, and in those case, you should know if it has a return
-public interface ExceptionalLamdaRet<R, TException extends Exception> {
+public interface SupplierEX<R, TException extends Exception> {
 
-
-    R execute() throws TException;
+    R get() throws TException;
 }
