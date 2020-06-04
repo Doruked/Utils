9986e195720805a16cd2d031d9e06befeef6ab6d
Doruked <44949043+Doruked@users.noreply.github.com>
2020-06-04 01:07:43 -0700

    Format code

--- src/test/java/com/example/doruked/responder/IntegrationTest.java
+++ src/test/java/com/example/doruked/responder/IntegrationTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.fail;
 
 public class IntegrationTest {
 
-
+    @SuppressWarnings("unchecked")
     public void test_integration_by_checking_observer_returns_the_expected_response() {
         Observer<List<Object>, List<Object>> observer = (e) -> { //an observer that adds a new object to notified list
             e.add(new Object());
@@ -19,18 +19,17 @@ public class IntegrationTest {
         List<Object> info = new ArrayList<>();
 
         try {
-            @SuppressWarnings("unchecked")
+            //mock mediator
             Mediator<List<Object>, List<Object>, List<Object>> mediator = (Mediator<List<Object>, List<Object>, List<Object>>) Mockito.mock(Mediator.class);
             Mockito.when(mediator.notify(info)).thenReturn(observer.process(info));
 
-            @SuppressWarnings("unchecked")
             Context<List<Object>> context = (Context<List<Object>>) Mockito.mock(Context.class);
             Mockito.when(context.getContext()).thenReturn(info);
 
+            //test
             int initial = info.size();
             List<Object> result = mediator.notify(context.getContext());
 
-            //assert values
             int current = result.size();
             assertEquals(initial + 1, current);
             fail("Result(" + result + ") did not contain a value where it should have");
@@ -38,8 +37,6 @@ public class IntegrationTest {
         } catch (InterruptedException e) {
             fail("Method should not be interrupted");
         }
-
-
     }
 
 }
