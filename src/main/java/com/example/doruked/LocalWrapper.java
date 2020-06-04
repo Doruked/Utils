386cce1efbb01ba8615643bfbf863a1d450528fe
Doruked <44949043+Doruked@users.noreply.github.com>
2020-06-04 00:14:05 -0700

    Reword documentation

--- src/main/java/com/example/doruked/LocalWrapper.java
+++ src/main/java/com/example/doruked/LocalWrapper.java
@@ -4,18 +4,12 @@ package com.example.doruked;
 import java.util.function.UnaryOperator;
 
 /**
- * An object wrapper.
- * It's intended use is as a local variable and being passed to nested methods who's behavior is known.
- * When used in such a way, this class imparts mutability on a variable of type {@link T} (who's type might
- * not be mutable), without the need of utilizing a return value after exiting various nested methods.
+ * An object wrapper used to somewhat emulate "passing by reference" in the java language.
+ * It is emulated by modifying the internal object when being passed.
  *
- * In this way, LocalWrapper is used to obfuscate, hide, reduce code duplication or make certain code
- * "cleaner" if such a form is desired.
- *
- * Note: Passing this object to another method should entail knowing what the destination method does with this object.
- * Publishing may lead to unaccounted changes. If unknown, it may be best to unwrap the object or not use the class.
- *
- * @implNote  more methods may be added, but only for adding flexible ways of achieving the standard behaviors
+ * @apiNote It's intended use is as a local variable and being passed to nested methods.
+ * The goal would be making some internal code cleaner/reducing duplication, not a general strategy.
+ * @implNote more methods may be added, but only for adding flexible ways of achieving the standard behaviors
  * which are to "set" and "get"
  */
 public final class LocalWrapper<T> {
