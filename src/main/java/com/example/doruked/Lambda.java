65a4c043dd7935595d1776439497c1df6931c378
Doruked <44949043+Doruked@users.noreply.github.com>
2020-06-04 01:21:06 -0700

    Remove comments that are useless
    removes old commented out code that won't be used

--- src/main/java/com/example/doruked/Lambda.java
+++ src/main/java/com/example/doruked/Lambda.java
@@ -21,13 +21,5 @@ public interface Lambda {
     static <T, U> Lambda build(T object1, U object2, BiConsumer<T, U> consumer){
         return ()-> consumer.accept(object1, object2);
     }
-////function //why would i convert function and not use it's value, in that case it should've been a consumer etc
-//    static <T, U,R> Lambda build(T object1, U object2, BiFunction<T, U,R> function){
-//        return ()-> function.apply(object1, object2);
-//    }
-//
-//    static <T,R> Lambda build(T object1, Function<T,R> function){
-//        return ()-> function.apply(object1);
-//    }
 
 }
