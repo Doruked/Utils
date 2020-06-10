21089d900bdab23288e25529afce6e889056fb3e
Doruked <44949043+Doruked@users.noreply.github.com>
2020-06-10 11:54:27 -0700

    Reword comment for consistency
    changed to use same wording as other methods

--- src/test/java/com/example/doruked/node/Iterators/DiveIteratorTest.java
+++ src/test/java/com/example/doruked/node/Iterators/DiveIteratorTest.java
@@ -63,7 +63,7 @@ public class DiveIteratorTest {
         //step 1: set initial childless node
         Node<Integer> initial = findChildlessNode(head);
 
-        //step 2: ensure initial is the last child
+        //step 2: ensure initial has next sibling
         trimTo(initial);
 
         //step 3: ensure parent has next sibling
